9a2492dda69453987a9ff0b13bebf28b67de3e6e
arroyhondo <garysanchez012@gmail.com>
2021-08-24 22:02:33 -0400

    hashed passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -60,6 +60,7 @@ router.put("/:id", (req, res) => {
 
   //if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     //UPDATE users
     //SET username = "username", email = "email", password = "password"
     //WHERE id = req.params.id
