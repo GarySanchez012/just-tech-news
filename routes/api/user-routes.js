595b5cd3bcf0a1dd58bb293de5decf6aa4070d95
arroyhondo <garysanchez012@gmail.com>
2021-08-25 00:11:53 -0400

    added password verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -54,6 +54,32 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  //Query operation
+  //expects {username: "username", email: "email", password: "password"}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+    //add comment syntax in front of this line in the .then()
+    // res.json({ user: dbUserData });
+
+    //verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
+
 //PUT /api/users/1
 router.put("/:id", (req, res) => {
   //expects {username: "username", email: "email", password: "password"}
